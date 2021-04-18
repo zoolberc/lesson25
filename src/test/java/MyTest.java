14691db0b9468cc3f482570643fe2e238c9b4e56
Aliev Tural <urre.2@ya.ru>
2021-04-19 02:10:18 +0300

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,27 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyTest {
+    public WebDriver driver;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void quit() {
+        driver.quit();
+    }
+
+
+    @Test
+    public void test1() {
+        driver.get("https://vk.com");
+    }
+}
