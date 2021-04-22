325910dfbde0184c0900df53eacf9909691d585c
Aliev Tural <urre.2@ya.ru>
2021-04-22 08:28:37 +0300

    Commit 10

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -31,6 +31,7 @@ public class MyTest {
         System.out.println("Hello Jenkins");
         System.out.println("Hello Jenkins");
         System.out.println("Hello Jenkins");
+        System.out.println("Hello Jenkins");
 
     }
 }
